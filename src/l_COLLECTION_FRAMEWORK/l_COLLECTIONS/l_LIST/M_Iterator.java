1a076bce51b73ff0b9ecbc6ba7ff86cf2d493f1c
shahsuvarasadzadeh <106305870+shahsuvarasadzadeh@users.noreply.github.com>
2023-03-08 22:52:29 +0400

    List Iterator method

--- src/l_COLLECTION_FRAMEWORK/l_COLLECTIONS/l_LIST/M_Iterator.java
+++ src/l_COLLECTION_FRAMEWORK/l_COLLECTIONS/l_LIST/M_Iterator.java
@@ -1,9 +1,15 @@
-package l_COLLECTION_FRAMEWORK;
+package l_COLLECTION_FRAMEWORK.l_COLLECTIONS.l_LIST;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 
-public class IteratorMethod {
+public class M_Iterator {
+    /** Iterator<E> iterator(): Returns an iterator over the elements in this list in proper sequence.
+     * Iterator<E> iterator():siyahıdakı elementlər üzərində müvafiq ardıcıllıqla iteratoru qaytarır.
+     * esasen hasNext() ve next() metodlarini isdifade edir.
+     * hasNext()-listin sonuna catib catmamaqi yoxlayir ,
+     * next()- listin bir sonraki elementi verir:
+     */
     public static void main(String[] args) {
         ArrayList<String> arrayList=new ArrayList<>();
         arrayList.add("Shahsuvar");
