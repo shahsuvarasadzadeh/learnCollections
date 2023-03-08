105e97d302ea894840f3fb7635ea7bcfab356a13
shahsuvarasadzadeh <106305870+shahsuvarasadzadeh@users.noreply.github.com>
2023-03-08 22:51:18 +0400

    List add method

---
+++ src/l_COLLECTION_FRAMEWORK/l_COLLECTIONS/l_LIST/M_add.java
@@ -0,0 +1,17 @@
+package l_COLLECTION_FRAMEWORK.l_COLLECTIONS.l_LIST;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class M_add {
+    /** boolean add(E e): Appends the specified element to the end of this list.
+     * verilen elementi listin sonuna elave edir */
+    public static void main(String[] args) {
+        List<String> list=new ArrayList<>();
+        list.add("Hello");
+        list.add("Salam");
+        for (String s:list){
+            System.out.println(s);
+        }
+    }
+}
