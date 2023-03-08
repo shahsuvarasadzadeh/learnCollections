1ed88596647c44e5269d1760e0464880e7efc143
shahsuvarasadzadeh <106305870+shahsuvarasadzadeh@users.noreply.github.com>
2023-03-08 22:51:42 +0400

    List Contains method

---
+++ src/l_COLLECTION_FRAMEWORK/l_COLLECTIONS/l_LIST/M_Contains.java
@@ -0,0 +1,17 @@
+package l_COLLECTION_FRAMEWORK.l_COLLECTIONS.l_LIST;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class M_Contains {
+    /** boolean contains(Object o): Returns true if this list contains the specified element. */
+    public static void main(String[] args) {
+        String[] stringList={"Alma","Heyva","Nar","Armud"};
+        List<String> list= Arrays.asList(stringList);
+        List<String> list1=new ArrayList<>();
+        System.out.println(list.contains("Heyva"));// true
+        System.out.println("---------");
+        System.out.println(list.contains("Qogal")); // false
+    }
+}
