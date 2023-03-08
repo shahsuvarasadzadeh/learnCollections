8f7a1b0137a318d370f72bbe1767503ec6c02e89
shahsuvarasadzadeh <106305870+shahsuvarasadzadeh@users.noreply.github.com>
2023-03-08 22:52:43 +0400

    List remove method

---
+++ src/l_COLLECTION_FRAMEWORK/l_COLLECTIONS/l_LIST/M_Remove.java
@@ -0,0 +1,28 @@
+package l_COLLECTION_FRAMEWORK.l_COLLECTIONS.l_LIST;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class M_Remove {
+    /**
+     * boolean remove(Object o): Removes the first occurrence of the specified element from this list.
+     * bu metodu biz her hansi bir Massivi
+     * Array.asList ile liste cevirdikde isdifade ede bilmerik hemcinin add mtodunu
+     * ve UnsupportedOperationException eception-nu alacayiq
+     */
+
+    public static void main(String[] args) {
+        //String[] stringList = {"Alma", "Heyva", "Nar", "Armud"};
+        //List<String> list1 = Arrays.asList(stringList);
+        //list1.add("hello");
+        //list1.remove(1)
+        List<String> list = new ArrayList<>();
+        list.add("hello");
+        list.add("hello1");
+        list.add("hello2");
+        System.out.println(list);
+        System.out.println(list.remove(1));
+        System.out.println(list);
+
+    }
+}
